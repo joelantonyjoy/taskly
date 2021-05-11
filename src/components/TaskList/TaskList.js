ca3fc65ce8e084d202ec9426757473602cb067d9
joelantonyjoy <joelantonyjoy@gmail.com>
2021-05-11 21:22:10 +0530

    Fix :Task filtering

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,12 +3,17 @@ import styles from './TaskList.module.css';
 import {TaskContext} from '../../contexts/TaskContext'
 
 const TaskList = () =>{
-    const [tasks] = useContext(TaskContext);
+    const [tasks,setTasks] = useContext(TaskContext);
+    const changeStatus = (modifiedTaskId) => {
+        setTasks((prev)=>{
+            return prev.map(a=>a.id===modifiedTaskId?{...a,isDone:!a.isDone}:{...a})
+        })
+    }
     return(
         <ul>
             {tasks.length>0?tasks.map(task => {
                 return(
-                    <li key={task.id} className={task.isDone?styles.done:styles.notDone}>{task.name} <span>{task.points} points</span></li>
+                    <li onClick={()=>{changeStatus(task.id)}} key={task.id} className={task.isDone?styles.done:styles.notDone}>{task.name} <span>{task.points} points</span></li>
                 );
             }):(
                 <div className={styles.noTask}>Try adding a task :)</div>
